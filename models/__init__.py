5b720b7e09bec48d8d1fd669430a77d485661ca9
JiaMingLin <robin890650@gmail.com>
2020-12-07 10:41:35 +0800

    mxnet import try

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,8 @@ from .rgb_vgg16 import *
 from .flow_vgg16 import *
 from .rgb_resnet import *
 from .flow_resnet import *
-from .flow_resnet_mx import *
+
+try:
+    from .flow_resnet_mx import *
+except:
+  	print("No mxnet installed")
