45a38621fdcddaa1c6123610bff26df5f73c26d2
JiaMingLin <robin890650@gmail.com>
2020-12-07 10:37:46 +0800

    no async

--- main_single_gpu.py
+++ main_single_gpu.py
@@ -219,8 +219,8 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
     for i, (input, target) in enumerate(train_loader):
 
-        input = input.float().cuda(async=True)
-        target = target.cuda(async=True)
+        input = input.float().cuda()
+        target = target.cuda()
         input_var = torch.autograd.Variable(input)
         target_var = torch.autograd.Variable(target)
 
@@ -267,8 +267,8 @@ def validate(val_loader, model, criterion):
 
     end = time.time()
     for i, (input, target) in enumerate(val_loader):
-        input = input.float().cuda(async=True)
-        target = target.cuda(async=True)
+        input = input.float().cuda()
+        target = target.cuda()
         input_var = torch.autograd.Variable(input, volatile=True)
         target_var = torch.autograd.Variable(target, volatile=True)
 
