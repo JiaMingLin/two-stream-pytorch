33b190dd1daef5577b57223574d4690fa6785bbd
JiaMingLin <robin890650@gmail.com>
2021-01-30 01:21:46 +0800

    add quantization

--- compression/quantization.py
+++ compression/quantization.py
@@ -103,6 +103,85 @@ def main():
     # Finally run results on the test set
     return app.test()
 
+def handle_subapps(model, criterion, optimizer, compression_scheduler, pylogger, args):
+    def load_test_data(args):
+        test_loader = classifier.load_data(args, load_train=False, load_val=False, load_test=True)
+        return test_loader
+
+    do_exit = False
+    if args.greedy:
+        greedy(model, criterion, optimizer, pylogger, args)
+        do_exit = True
+    elif args.summary:
+        # This sample application can be invoked to produce various summary reports
+        for summary in args.summary:
+            distiller.model_summary(model, summary, args.dataset)
+        do_exit = True
+    elif args.export_onnx is not None:
+        distiller.export_img_classifier_to_onnx(model,
+                                                os.path.join(msglogger.logdir, args.export_onnx),
+                                                args.dataset, add_softmax=True, verbose=False)
+        do_exit = True
+    elif args.qe_calibration and not (args.evaluate and args.quantize_eval):
+        classifier.acts_quant_stats_collection(model, criterion, pylogger, args, save_to_file=True)
+        do_exit = True
+    elif args.activation_histograms:
+        classifier.acts_histogram_collection(model, criterion, pylogger, args)
+        do_exit = True
+    elif args.sensitivity is not None:
+        test_loader = load_test_data(args)
+        sensitivities = np.arange(*args.sensitivity_range)
+        sensitivity_analysis(model, criterion, test_loader, pylogger, args, sensitivities)
+        do_exit = True
+    elif args.evaluate:
+        if args.quantize_eval and args.qe_lapq:
+            image_classifier_ptq_lapq(model, criterion, pylogger, args)
+        else:
+            test_loader = load_test_data(args)
+            classifier.evaluate_model(test_loader, model, criterion, pylogger,
+                classifier.create_activation_stats_collectors(model, *args.activation_stats),
+                args, scheduler=compression_scheduler)
+        do_exit = True
+    elif args.thinnify:
+        assert args.resumed_checkpoint_path is not None, \
+            "You must use --resume-from to provide a checkpoint file to thinnify"
+        distiller.contract_model(model, compression_scheduler.zeros_mask_dict, args.arch, args.dataset, optimizer=None)
+        apputils.save_checkpoint(0, args.arch, model, optimizer=None, scheduler=compression_scheduler,
+                                 name="{}_thinned".format(args.resumed_checkpoint_path.replace(".pth.tar", "")),
+                                 dir=msglogger.logdir)
+        msglogger.info("Note: if your model collapsed to random inference, you may want to fine-tune")
+        do_exit = True
+    return do_exit
+
+
+def init_knowledge_distillation(args, model, compression_scheduler):
+    args.kd_policy = None
+    if args.kd_teacher:
+        teacher = create_model(args.kd_pretrained, args.dataset, args.kd_teacher, device_ids=args.gpus)
+        if args.kd_resume:
+            teacher = apputils.load_lean_checkpoint(teacher, args.kd_resume)
+        dlw = distiller.DistillationLossWeights(args.kd_distill_wt, args.kd_student_wt, args.kd_teacher_wt)
+        args.kd_policy = distiller.KnowledgeDistillationPolicy(model, teacher, args.kd_temp, dlw)
+        compression_scheduler.add_policy(args.kd_policy, starting_epoch=args.kd_start_epoch, ending_epoch=args.epochs,
+                                         frequency=1)
+        msglogger.info('\nStudent-Teacher knowledge distillation enabled:')
+        msglogger.info('\tTeacher Model: %s', args.kd_teacher)
+        msglogger.info('\tTemperature: %s', args.kd_temp)
+        msglogger.info('\tLoss Weights (distillation | student | teacher): %s',
+                       ' | '.join(['{:.2f}'.format(val) for val in dlw]))
+        msglogger.info('\tStarting from Epoch: %s', args.kd_start_epoch)
+
+
+def early_exit_init(args):
+    if not args.earlyexit_thresholds:
+        return
+    args.num_exits = len(args.earlyexit_thresholds) + 1
+    args.loss_exits = [0] * args.num_exits
+    args.losses_exits = []
+    args.exiterrors = []
+    msglogger.info('=> using early-exit threshold values of %s', args.earlyexit_thresholds)
+
+
 class ActionRecognizerCompressor(classifier.ClassifierCompressor):
     def __init__(self, args, script_dir):
         super().__init__(args, script_dir)
